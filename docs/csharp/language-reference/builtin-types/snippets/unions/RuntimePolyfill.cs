3a390da6639cb953c68b6116e5fc09a4bea275b2
Bill Wagner <wiwagn@microsoft.com>
2026-03-20 14:49:55 -0400

    use polyfill snippet

--- docs/csharp/language-reference/builtin-types/snippets/unions/RuntimePolyfill.cs
+++ docs/csharp/language-reference/builtin-types/snippets/unions/RuntimePolyfill.cs
@@ -1,4 +1,5 @@
 // Remove this file when UnionAttribute and IUnion are included in the .NET runtime.
+// <snippet_RuntimePolyfill>
 namespace System.Runtime.CompilerServices
 {
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = false)]
@@ -9,3 +10,4 @@ namespace System.Runtime.CompilerServices
         object? Value { get; }
     }
 }
+// </snippet_RuntimePolyfill>
